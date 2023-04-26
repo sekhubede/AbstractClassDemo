46241eec8248bc6340c3110ebb49c76bad81f6c5
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-26 16:29:58 +0200

    Added a Cube class with some logic.
    The Cube class is derived from the Sphere abstract class.

---
+++ ConsoleUI/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class Cube : Shape
+    {
+        public double Length { get; set; }
+
+        public Cube(double length)
+        {
+            this.Name = "Cube";
+            this.Length = length;
+        }
+
+        public override double Volume()
+        {
+            return Math.Pow(Length, 3);
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            WriteLine($"The Cube has a length of {Length}");
+        }
+    }
+}
