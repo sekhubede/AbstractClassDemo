b9648200febb566a49161929a9152afb3cbdece3
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-26 16:28:05 +0200

    Added a Sphere abstract class with some logic

---
+++ ConsoleUI/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    // abstract, means that this class cannot be instantiated.
+    // objects of this class cannot be created.
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public virtual void GetInfo()
+        {
+            WriteLine($"\nThis is a {Name}");
+        }
+
+        // volume method not implemented.
+        // indicates that the deriving classes should implement
+        // a functionality for a Volume method.
+        public abstract double Volume();
+    }
+}
