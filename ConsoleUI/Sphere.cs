6660335eb9ffc6c1bdb48fa982a0619f26c141fd
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-26 16:29:09 +0200

    Added a Sphere class with some logic
    The Sphere class is deriving from the Shape abstract class.

---
+++ ConsoleUI/Sphere.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class Sphere : Shape
+    {
+        public double Radius { get; set; }
+
+        public Sphere(double radius)
+        {
+            this.Name = "Sphere";
+            this.Radius = radius;
+        }
+
+        public override double Volume()
+        {
+            return Math.PI * (Math.Pow(Radius, 3)) * 4 / 3;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            WriteLine($"The Sphere has a Radius of {Radius}");
+        }
+    }
+}
